a853e03ce30a883a9a8c0e61759d054a677c7fb7
Echabu <88000163+Echabu@users.noreply.github.com>
2026-06-09 23:08:48 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 820,
+    resizable: false,
+    webPreferences: {
+      contextIsolation: true,
+      sandbox: false
+    }
+  });
+
+  win.removeMenu();
+  win.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
